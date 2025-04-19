4fb5cb095dc1f0309fc1b1b4c2ff4ac72e87bc57
shobhitlal1 <shobhitblal1234@gmail.com>
2025-04-19 14:53:27 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/styles/globals.css'
-import Cursor from '@/components/Cursor'
+import '../styles/globals.css'
+import Cursor from '../components/Cursor'
 
 export default function App({ Component, pageProps }) {
   return (
